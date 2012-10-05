2cf9c3171349f52f614462b4aaf313b56a109908
Yali Sassoon <yali.sassoon@keplarllp.com>
2012-10-05 11:25:54 +0100

    updated yaml file and block for processing yaml

---
+++ search-engines-lookup.rb
@@ -0,0 +1,19 @@
+require 'yaml'
+
+se = YAML.load_file('search_engines.yml')
+
+# Create a hash of search engine domains, that we will perform lookups 
+# to test if referrers are search engines and look up the corresponding query parameter
+
+search_engine_domain_lookups = Hash.new # blank map to start with
+
+se.each do | search_engine_name, search_engine_data |
+	search_engine_data['domains'].each do | domain |
+		puts domain + " " + search_engine_name + " " + search_engine_data['parameter']
+		new_domain = { domain => { "name" => search_engine_name, "parameter" => search_engine_data['parameter'] } }
+		search_engine_domain_lookups.merge!(new_domain) 
+	end
+end
+
+puts search_engine_domain_lookups
+search_engine_domain_lookups.each { |domain, data| puts domain + " " + data['name'] + " " + data['parameter'] }
