980490521c14f3e5a755a95c06a9d6269b442919
Sepp Wijnands <sw@iperform.nl>
2013-07-07 12:24:44 +0200

    Added .NET implementation.

---
+++ dotnet/RefererParser/RefererDefinition.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RefererParser
+{
+    /// <summary>
+    /// Referer definition for a single referer.
+    /// </summary>
+    public class RefererDefinition
+    {
+        /// <summary>
+        /// Medium of definition
+        /// </summary>
+        public RefererMedium Medium
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Name of referer
+        /// </summary>
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Domains belonging to this referer
+        /// </summary>
+        public string[] Domains
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Search parameter names in referer request string
+        /// </summary>
+        public string[] Parameters
+        {
+            get;
+            set;
+        }
+    }
+}
