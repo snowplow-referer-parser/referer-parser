c8c0f34ef98813718b4037343f14d0e215a634bc
emilssolmanis <emils.solmanis@gmail.com>
2013-07-31 17:57:56 +0300

    updated Python referer_parser to match Java version, added tests

--- python/referer_parser/__init__.py
+++ python/referer_parser/__init__.py
@@ -5,28 +5,91 @@ from urlparse import urlparse, parse_qsl
 
 JSON_FILE = os.path.join(os.path.dirname(__file__), 'data', 'referers.json')
 REFERERS = {}
-for ref, config in json.load(open(JSON_FILE))['search'].iteritems(): # TODO: update this to support the other referer mediums as well
-    for domain in config['domains']:
-        REFERERS[domain] = {
-            'name': ref,
-            'params': map(unicode.lower, config['parameters']),
-        }
+
+with open(JSON_FILE) as json_content:
+    for medium, conf_list in json.load(json_content).iteritems():
+        for ref, config in conf_list.iteritems():
+            make_ref = None
+            if 'parameters' in config:
+                def make_ref_params(config_dict):
+                    return {
+                        'name': ref,
+                        'params': map(unicode.lower, config_dict['parameters']),
+                        'medium': medium,
+                    }
+                make_ref = make_ref_params
+            else:
+                make_ref = lambda _: {'name': ref, 'medium': medium}
+            for domain in config['domains']:
+                REFERERS[domain] = make_ref(config)
 
 
 class Referer(object):
-    def __init__(self, url):
-        self.uri = urlparse(url)
-        host = self.uri.netloc.split(':', 1)[0]
-        hostpath = host + self.uri.path
-        if hostpath in REFERERS:
-            host = hostpath
-        self.known = False if host not in REFERERS else True
+    def __init__(self, ref_url, curr_url=None):
+        self.known = False
         self.referer = None
-        self.search_parameter = ''
-        self.search_term = ''
-        if self.known:
-            self.referer = REFERERS[host]['name']
-            for param, val in parse_qsl(self.uri.query):
-                if param.lower() in REFERERS[host]['params']:
+        self.medium = 'unknown'
+        self.search_parameter = None
+        self.search_term = None
+
+        ref_uri = urlparse(ref_url)
+        ref_host = ref_uri.hostname
+        self.known = ref_uri.scheme in {'http', 'https'}
+
+        # print "Scheme: %s" % ref_uri.scheme
+
+        if not self.known:
+            return
+
+        if curr_url:
+            curr_uri = urlparse(curr_url)
+            curr_host = curr_uri.hostname
+            if curr_host == ref_host:
+                self.medium = 'internal'
+                return
+
+        # print "Getting referer with path"
+        referer = self.__lookup_referer(ref_host, ref_uri.path, True)
+        # print "Got %s" % referer
+        if not referer:
+            # print "Getting referer without path"
+            referer = self.__lookup_referer(ref_host, ref_uri.path, False)
+            # print "Got %s" % referer
+            if not referer:
+                self.medium = 'unknown'
+                return
+
+        # print "Assigning name %s" % referer['name']
+        self.referer = referer['name']
+        self.medium = referer['medium']
+        
+        if referer['medium'] == 'search':
+            if 'params' not in referer or not referer['params']:
+                # print "Returning"
+                return
+            for param, val in parse_qsl(ref_uri.query):
+                if param.lower() in referer['params']:
                     self.search_parameter = param
                     self.search_term = val
+
+    def __lookup_referer(self, ref_host, ref_path, include_path):
+        referer = None
+        try:
+            referer = REFERERS[ref_host + ref_path] if include_path else REFERERS[ref_host]
+        except KeyError:
+            if include_path:
+                path_parts = ref_path.split('/')
+                if len(path_parts) > 1:
+                    try:
+                        referer = REFERERS[ref_host + '/' + path_parts[1]]
+                    except KeyError:
+                        pass
+        if not referer:
+            try:
+                idx = ref_host.index('.')
+                return self.__lookup_referer(ref_host[idx + 1:], ref_path, include_path)
+            except ValueError:
+                return None
+        else:
+            return referer
+        
