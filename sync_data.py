b37b3180957edcb57d60d857821721c647d81283
Alex Dean <alexander.dean@keplarllp.com>
2013-11-11 08:59:55 +0000

    Added copyright & license

--- sync_data.py
+++ sync_data.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2013 Martin Katrenik, Snowplow Analytics Ltd. All rights reserved.
+#
+# This program is licensed to you under the Apache License Version 2.0,
+# and you may not use this file except in compliance with the Apache
+# License Version 2.0.
+# You may obtain a copy of the Apache License Version 2.0 at
+# http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the Apache License Version 2.0 is
+# distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied.
+# See the Apache License Version 2.0 for the specific language
+# governing permissions and limitations there under.
+
+# Authors::   Martin Katrenik, Alex Dean (mailto:support@snowplowanalytics.com)
+# Copyright:: Copyright (c) 2013 Martin Katrenik, Snowplow Analytics Ltd
+# License::   Apache License Version 2.0
+
 # Syncs common referer-parser resources to the
 # language-specific sub projects.
 #
