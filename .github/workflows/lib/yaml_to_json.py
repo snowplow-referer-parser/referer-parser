8e26aa661ccc11ed3363d31dba86669729961b97
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2023-03-14 20:28:11 +0100

    debug

--- .github/workflows/lib/yaml_to_json.py
+++ .github/workflows/lib/yaml_to_json.py
@@ -1,30 +1,9 @@
-#!/bin/bash
-
-# PROPRIETARY AND CONFIDENTIAL
-#
-# Unauthorized copying of this project via any medium is strictly prohibited.
-#
-# Copyright (c) 2020-2020 Snowplow Analytics Ltd. All rights reserved.
-
-# Generate the JSON equivalent of the yaml file with the list of referers
-
 import sys
-import json
 import yaml
+import json
 
-if len(sys.argv) != 3:
-    print 'USAGE: yaml_to_json.py <input_referers.yaml> <output_referers.json>'
-    print '2 arguments must be specified'
-    print 'Arguments:'
-    print sys.argv[1:]
-    sys.exit(1)
+with open(sys.argv[1], 'r') as input:
+    yml = yaml.safe_load(input)
 
-input_yaml = sys.argv[1]
-output_json = sys.argv[2]
-
-referers = yaml.load(open(input_yaml))
-json = json.dumps(referers, sort_keys = False, indent = 4)
-
-print "writing to {0} ".format(output_json)
-with open(output_json, 'w') as f:
-    f.write(json)
+with open(sys.argv[2], 'w') as output:
+    json.dump(yml, output)
